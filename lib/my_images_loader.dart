965721d92f8fbf60fdf218daf234bc90acb0f37e
muhammednashat <mnashat.dev@gmail.com>
2025-01-29 11:07:25 +0200

    modified my_images_loader.dart

--- lib/my_images_loader.dart
+++ lib/my_images_loader.dart
@@ -2,16 +2,16 @@
 class MyImagesLoader{
 
 
-     static String activity1 = 'calender_activity_1.png';
-     static String activity12 = 'calender_activity_1.png';
-     static String activity13 = 'calender_activity_1.png';
-     static String activity14 = 'calender_activity_1.png';
-     static String activity15 = 'calender_activity_1.png';
-     static String activity16 = 'calender_activity_1.png';
-     static String activity17 = 'calender_activity_1.png';
-     static String activity18 = 'calender_activity_1.png';
-     static String activity19 = 'calender_activity_1.png';
-     static String activity20 = 'calender_activity_1.png';
+     static String activity1 = 'assets/calender_activity_1.png';
+     static String activity12 = 'assets/calender_activity_1.png';
+     static String activity13 = 'assets/calender_activity_1.png';
+     static String activity14 = 'assets/calender_activity_1.png';
+     static String activity15 = 'assets/calender_activity_1.png';
+     static String activity16 = 'assets/calender_activity_1.png';
+     static String activity17 = 'assets/calender_activity_1.png';
+     static String activity18 = 'assets/calender_activity_1.png';
+     static String activity19 = 'assets/calender_activity_1.png';
+     static String activity20 = 'assets/calender_activity_1.png';
 
      
 } 
