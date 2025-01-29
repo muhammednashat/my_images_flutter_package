8a79635cc2243eeab02e1d6767cd4e5eef3874b9
muhammednashat <mnashat.dev@gmail.com>
2025-01-29 10:24:34 +0200

    initial commit

---
+++ lib/my_images_loader.dart
@@ -0,0 +1,17 @@
+
+class MyImagesLoader{
+
+
+     static String activity1 = 'calender_activity_1.png';
+     static String activity12 = 'calender_activity_1.png';
+     static String activity13 = 'calender_activity_1.png';
+     static String activity14 = 'calender_activity_1.png';
+     static String activity15 = 'calender_activity_1.png';
+     static String activity16 = 'calender_activity_1.png';
+     static String activity17 = 'calender_activity_1.png';
+     static String activity18 = 'calender_activity_1.png';
+     static String activity19 = 'calender_activity_1.png';
+     static String activity20 = 'calender_activity_1.png';
+
+     
+} 
